b5d269a44a05f6f2c394d3bd69ed5c6bdb7d0ebc
Paulo Cesar Fordelone <pc@fordelone.com.br>
2022-08-04 13:00:07 -0300

    - first commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#FAFAFA',
+  'gray-200': '#F3F2F2',
+  'gray-300': '#EDEDED',
+  'gray-400': '#E6E5E5',
+  'gray-500': '#D7D5D5',
+  'gray-600': '#8D8686',
+  'gray-700': '#574F4D',
+  'gray-800': '#403937',
+  'gray-900': '#272221',
+
+  'purple-100': '#EBE5F9',
+  'purple-300': '#8047F8',
+  'purple-500': '#4B2995',
+
+  'yellow-300': '#FBA94C',
+  'yellow-500': '#FBA94C',
+
+  'orange-500': '#FBA94C',
+}
